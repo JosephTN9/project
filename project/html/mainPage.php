793abddedfd4cb191f1fa067186fcecd411483b5
JosephTN9 <42596499+JosephTN9@users.noreply.github.com>
2018-08-22 00:00:36 -0400

    Add files via upload

---
+++ project/html/mainPage.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Weeaboo4U - Anime Figures & Hobby Shop~</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<link href="../css/style.css" rel="stylesheet" type="text/css">
+<script src="../js/sitejs.js"></script>
+
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+
+<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+
+</head>
+
+
+
+
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+<div class="jumbotron text-center">
+<h1><strong>Weeaboo4U</strong></h1> 
+<p>Anime Figures & Hobby Shop~</p> 
+</div>
+
+
+
+
+
+<div class="container-fluid bg-white">
+<div class="container-fluid bg-white text-center" id="about">
+<div class="row">
+<div class="col-sm-8">
+<div class="logo">
+<h2>What is Weeaboo4U</h2>
+</div>
+<h4>Weeaboo4U is a one stop shop that makes it easy for people to sell their Anime Figures online!</h4>
+<h4>Whether you are looking to sell old figurines, buy a new one, </h4>
+<h4>or are searching for that one rare figure, W4U can do it all for you! ^_^ </h4> 
+
+<p>&#51060;&#48120; &#45212; &#45796; &#52984;&#45796;&#44256; &#49373;&#44033;&#54616;&#45716;&#45936;</p>
+<p>&#50612;&#51788;&#47732; &#45236; &#47576;&#51064;&#45936; &#50780;</p>
+<p>&#45236; &#47576;&#45824;&#47196; &#54624; &#49688; &#50630;&#45716; &#44148;&#45936;</p>
+<p>&#48128;&#50612;&#45236;&#47140;&#44256; &#54616;&#47732; &#54624;&#49688;&#47197;</p>
+<p>&#51088;&#44984; &#45132;&#47140; &#50780; &#51088;&#44984; &#51088;&#44984; &#45132;&#47140; baby</p>
+<p>I'm like TT, just like TT</p>
+<p>&#51060;&#47088; &#45236; &#47576; &#47784;&#47476;&#44256; &#45320;&#47924;&#54644; &#45320;&#47924;&#54644;</p>
+<p>I'm like TT, just like TT</p>
+<p>Tell me that you'll be my baby</p>
+</div>
+
+
+
+
+
+<div class="col-sm-4">
+<span>
+<img src="../images/whatIsPicture.jpg" alt="Black Rock Shooter - The Game - Black Rock Shooter - Figma #116 (Max Factory)" class="center">
+</span> 
+</div>
+</div>
+
+<div class="container-fluid bg-grey" id="inventory">
+<div class="text-center">
+<div class="logo">  
+<h2>Inventory</h2>
+</div>
+  <h4>Figures in Stock</h4>
+  <div class="row text-center">
+    <div class="col-sm-4">
+      <div class="thumbnail">
+        <img src="../images/15.jpg" alt="Steins;Gate - Makise Kurisu - Figma #098 (Max Factory)">
+        <p><strong>Steins;Gate - Makise Kurisu</strong></p>
+        <p>Figma #098 (Max Factory)</p>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="thumbnail">
+        <img src="../images/16.jpg" alt="Nisemonogatari - Senjougahara Hitagi (Taito)">
+        <p><strong>Nisemonogatari - Senjougahara Hitagi</strong></p>
+        <p>(Taito)</p>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="thumbnail">
+        <img src="../images/18.jpg" alt="Bakemonogatari - Sengoku Nadeko - High Grade Figure (SEGA)">
+        <p><strong>Bakemonogatari - Sengoku Nadeko</strong></p>
+        <p>High Grade Figure (SEGA)</p>
+      </div>
+    </div>
+</div>
+</div>
+</div>
+
+
+
+
+
+<div class="container-fluid bg-white" id="add">
+<div class="logo">
+ <h2>Add a Figure to Sell</h2>
+
+<form action="mainPage.php" method="post"><pre>
+  Product Name <input type="text" name="productName">
+        Series <input type="text" name="series">
+  Manufacturer <input type="text" name="manufacturer">
+ Selling Price <input type="text" name="sellingPrice">
+   Seller Name <input type="text" name="sellerName">
+Contact E-mail <input type="text" name="email">
+Figure Picture <input type="text" name="figurePicture">
+<input type="submit" value="ADD FIGURE">
+</pre></form>
+</div>
+</div>
+
+
+
+
+
+<div class="container-fluid bg-grey" id="reviews">
+<div class="logo">
+<h2>What our customers say</h2>
+</div>
+<div id="myCarousel" class="carousel slide text-center" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+    <h4>"That store boomed me. He's so good! (x4)"<br><span style="font-style:normal;">LeBron James, Los Angeles Lakers</span></h4>
+    </div>
+    <div class="item">
+      <h4>"I'll be back!"<br><span style="font-style:normal;">Arnold Schwarzenegger, The Terminator</span></h4>
+    </div>
+    <div class="item">
+      <h4>"WHERE ARE THEY!?"<br><span style="font-style:normal;">Bruce Wayne, The Batman</span></h4>
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
+
+
+
+
+
+
+<div class="container-fluid bg-white" id="contact">
+<div class="container-fluid">
+<div class="logo">
+  <h2 class="text-center">CONTACT</h2>
+</div>
+  <div class="row">
+    <div class="col-sm-5">
+      <p>Contact us and we'll get back to you within 24 hours.</p>
+      <p><span class="glyphicon glyphicon-map-marker"></span> Windsor, ON</p>
+      <p><span class="glyphicon glyphicon-phone"></span> 310-1010</p>
+      <p><span class="glyphicon glyphicon-envelope"></span> 123fake@email.com</p> 
+    </div>
+    <div class="col-sm-7">
+      <div class="row">
+        <div class="col-sm-6 form-group">
+          <input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
+        </div>
+        <div class="col-sm-6 form-group">
+          <input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
+        </div>
+      </div>
+      <textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5"></textarea><br>
+      <div class="row">
+        <div class="col-sm-12 form-group">
+          <button class="btn btn-default pull-right" type="submit">Send</button>
+        </div>
+      </div> 
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+
+
+
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+      <a class="navbar-brand" href="#">Weeaboo4U</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#about">ABOUT</a></li>
+        <li><a href="#inventory">INVENTORY</a></li>
+        <li><a href="#add">ADD</a></li>
+        <li><a href="#reviews">REVIEWS</a></li>
+        <li><a href="#contact">CONTACT</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+
+
+</body>
+</html>
